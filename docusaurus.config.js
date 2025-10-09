db98a1ceb9363f010413d37048401f4e260326ef
xenthrall <xenthrall0@gmail.com>
2025-10-09 17:48:32 -0500

    domain: agrega baseUrl = /

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://jhontequia.github.io',
+  url: 'http://docs.cronosena.site/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/CronoSENA-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
